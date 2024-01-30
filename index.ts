15af43474a054690b886d1ec03690041ab33957a
Mariane <marianecarvalho@ibm.com>
2024-01-30 11:21:38 -0300

    setup

---
+++ index.ts
@@ -0,0 +1,7 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+
+const purchaseUrl = 'https://run.mocky.io/v3/37105b2e-7783-4133-bbd4-632503347c95';
+const clientsUrl = 'https://run.mocky.io/v3/e55a624b-0588-4e7a-a869-e4ed921e35ea';
